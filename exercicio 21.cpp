361b25b3c49bdacd55e38dfc461b25837cfd5aca
Laboratorio XXIA - 104 <lab104a@lab.unesc.net>
2024-03-19 20:32:26 -0300

    exercicio 21

---
+++ exercicio 21.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+main()
+{
+	int n1;
+	int n2;
+	float soma;
+	float mult;
+	
+	printf("digite n1:\n");
+	scanf("%d", &n1);
+	
+	printf("digite n2:\n");
+	scanf("%d", &n2);
+	
+	printf("numero 1: %d\n", n1);
+	
+	printf("numero 2: %d\n", n2);
+	
+	soma=n1+n2;
+	
+	printf("soma= %f\n", soma);
+	
+	mult=n1*n2;
+	
+	printf("mult= %f\n", mult);
+	
+	
+	
+}
