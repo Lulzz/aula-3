0e86068fe30a49aca1fe98034e8318ed691656c4
Laboratorio XXIA - 104 <lab104a@lab.unesc.net>
2024-03-19 19:32:20 -0300

    atividade 17

---
+++ atividade 17.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+main(){
+	float idade;
+	char nome[30];
+	
+	printf("qual a idade do caboclo;\n");
+	scanf("%f", &idade);
+	
+	printf("qual o nome do cabocl0:\n");
+	scanf("%s", &nome);
+	
+	printf("nome: %f\n", idade);
+	 
+	printf("idade: %s", nome);
+	
+	
+	
+	
+	
+	
+	
+	
+}
